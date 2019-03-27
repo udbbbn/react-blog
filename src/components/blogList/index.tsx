8c59c49a5aebaef617fa64287ceda628fe2ce965
udbbbn <udbbbn@163.com>
2019-03-27 14:45:27 +0800

    test

---
+++ src/components/blogList/index.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import * as s from './index.less';
+
+export default class blogList extends Component {
+  render() {
+    return (
+      <div>
+        blogList
+      </div>
+    )
+  }
+}
