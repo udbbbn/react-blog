b281555930d9e664d44fbe76bcad331829ded487
udbbbn <udbbbn@163.com>
2019-04-12 11:28:58 +0800

    mongo find+limit分页方案行不通 换成skip+limit

--- src/components/blogList/index.tsx
+++ src/components/blogList/index.tsx
@@ -5,6 +5,7 @@ import { createHashHistory as history } from "history";
 import { Router, Route } from "react-router-dom";
 import BlogDetail from "../blogDetail";
 import { FormComponentProps } from "antd/lib/form";
+import request from "superagent";
 
 const Search = Input.Search;
 
@@ -15,12 +16,25 @@ interface I_Icon {
 }
 interface I_State {
   showDetail: boolean;
-  _id: string
+  _id: string,
+  blogList: Array<object>,
+  nextDataId: string,  // mongo 使用find+limit分页需要一个id索引
+  prevDataId: string, // 用于上一页
+  listFirstId: string, // 每页第一条id
+  prevPage: number, // 上一个页码
+  allCount: number,
+  pageSize: number
 }
 interface IProps extends FormComponentProps {
   history: History;
   location: Location;
   [name: string]: any;
+}
+interface I_BlogList {
+  title: string,
+  fileName: string,
+  description: string,
+  date: string
 }
 
 const IconText = ({ type, text, key }: I_Icon) => (
@@ -35,14 +49,27 @@ export default class blogList extends Component<IProps, I_State> {
     super(props);
     this.state = {
       showDetail: false,
-      _id: null
+      _id: null,
+      nextDataId: '',
+      prevDataId: '',
+      listFirstId: '',
+      prevPage: 1,
+      blogList: [],
+      allCount: 10,
+      pageSize: 5
     };
   }
 
+  componentWillMount() {
+    this.fetchData();
+  }
   componentDidMount() {
     this.judgePrams();
   }
 
+  /**
+   * 获取参数更换视图
+   */
   judgePrams() {
     if (this.props.location.search) {
       let id = this.props.location.search.split("id=")[1];
@@ -52,22 +79,72 @@ export default class blogList extends Component<IProps, I_State> {
       });
     }
   }
-
-  pageChange(page: number, pageSize: number) {
-    console.log(page, pageSize);
+  
+  /**
+   * 获取blog列表
+   * @param _id string 当前数据列表最后一条或第一条 用于翻页 
+   * @param pageState boolean 判断是上一页还是下一页
+   */
+  fetchData(_id = '', page = 0) {
+    const vm = this;
+    request
+      .get(`http://127.0.0.1:3000/blogDetail?pageSize=${vm.state.pageSize}&page=${page}`)
+      .end(function (err, res: any) {
+        if (err) {
+        } else {
+          if (res.body.code === 200) {
+            const data = res.body.data;
+            vm.setState({
+              blogList: data,
+              listFirstId: data[0]._id,
+              nextDataId: data[data.length - 1]._id,
+              allCount: res.body.count
+            })
+          }
+        }
+      });
   }
 
+  /**
+   * 更换页码
+   */
+  pageChange = (page: number, pageSize: number) => {
+    // let pageState: boolean; //判断是上一页(false)还是下一页(true)
+
+    // // 如果是非连续页数 如 1 3 5 使用另一种分页方式 详见后端代码
+    // if (this.state.prevPage + 1 != page && this.state.prevPage - 1 != page) {
+    //   if (this.state.prevPage < page) { // 用于区分mongo列表排序方式 怎么这么麻烦!! 不如sql
+    //     pageState = true;
+    //   } else {
+    //     pageState = false;
+    //   }
+    //   this.fetchData('', pageState, 'http://127.0.0.1:3000/blogDetailSkip', page)
+    // } else {
+    //   // 若为连续页码 如 1 2 3 4 
+    //   if (this.state.prevPage < page) { // 下一页
+    //       pageState = true;
+    //       const firstId = this.state.listFirstId;
+    //       this.setState({
+    //         prevDataId: firstId
+    //       })
+    //       this.fetchData(this.state.nextDataId, pageState)
+    //     } else { // 上一页
+    //       pageState = false;
+    //       this.fetchData(this.state.prevDataId, pageState)
+    //     }
+    // }
+    // this.setState({
+    //   prevPage: page
+    // })
+      this.fetchData(this.state.nextDataId, page - 1)
+  }
+
+  /**
+   * 更改视图
+   */
   blogDetail = (e: MouseEvent<HTMLElement>) => {
     const key = (e.currentTarget as HTMLElement).dataset.key;
-    // this.setState(
-    //   {
-    //     showDetail: true
-    //   },
-    //   () => {
-    //     history().push("/blogDetail?id=" + key);
-    //   }
-    // );
-      history().push("/blogDetail?id=" + key);
+    history().push("/blogDetail?id=" + key);
   };
 
 
@@ -77,37 +154,31 @@ export default class blogList extends Component<IProps, I_State> {
   renderList() {
     return (
       <div>
-        {[1, 2, 3, 4, 5].map((el, index) => {
+        {this.state.blogList.map((el: I_BlogList, index) => {
           return (
             <div
               className={s.blog + " flex-c-c"}
               key={index}
-              data-key={index}
+              data-key={el.fileName}
               onClick={this.blogDetail}
             >
               <div>
                 <img className={s.img} src={require("img/bg.jpg")} alt="" />
               </div>
               <div className={s.content + " flex-column-between"}>
-                <div className={s.title + " two-ellipsis"}>
-                  这篇文章主要是测试文字过长后显示的省略号效果
-                  这篇文章主要是测试文字过长后显示的省略号效果
-                  这篇文章主要是测试文字过长后显示的省略号效果
+                <div className={s.title + " two-ellipsis"}>{el.title}
                 </div>
-                <div className={s.description + " two-ellipsis"}>
-                  这段描述要长一点 因为要测试省略号效果 这段描述要长一点
-                  因为要测试省略号效果 这段描述要长一点 因为要测试省略号效果
-                  这段描述要长一点 因为要测试省略号效果
+                <div className={s.description + " two-ellipsis"}>{el.description}
                 </div>
                 <div className={s.iconArea}>
                   <span>
                     <Icon type="clock-circle" style={{ marginRight: 8 }} />
-                    {new Date().getTime()}
+                    {el.date}
                   </span>
-                  <span>
+                  {/* <span>
                     <Icon type="message" style={{ marginRight: 8 }} />
                     {2}
-                  </span>
+                  </span> */}
                 </div>
               </div>
             </div>
@@ -116,7 +187,8 @@ export default class blogList extends Component<IProps, I_State> {
         <Pagination
           className={s.pagination}
           defaultCurrent={1}
-          total={50}
+          defaultPageSize={this.state.pageSize}
+          total={this.state.allCount}
           onChange={this.pageChange}
         />
       </div>
